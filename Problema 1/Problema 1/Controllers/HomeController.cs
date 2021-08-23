b30204ea8c5122206f04fa830edea9074ebf1740
Leonel-H29 <leonelherreradkno@gmail.com>
2021-08-23 12:10:08 -0300

    Agrego el Problema 1 resuelto

---
+++ Problema 1/Problema 1/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Problema_1.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Problema_1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        
+        public double Problema1(int num)
+        {
+            return Math.Pow(num, 2);
+        }
+        
+        //https://localhost:44323/Home/Problema1?num=2
+        /*
+        public IActionResult Problema1(int num)
+        {
+            return View();
+        }
+        */
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
